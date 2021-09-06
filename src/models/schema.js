ce623ceffeb7055741b3409e006ef32daadcdd0a
ArturSzmidt <80460700+ArturSzmidt@users.noreply.github.com>
2021-09-06 12:19:01 +0200

    schema

---
+++ src/models/schema.js
@@ -0,0 +1,35 @@
+import mongoose from 'mongoose';
+
+const { Schema, model } = mongoose;
+
+export const MessageSchema = new Schema({
+  text: {
+    type: String,
+    required: true,
+  },
+  sender: {
+    type: String,
+    required: true,
+  },
+  id: {
+    type: String,
+    required: true,
+  },
+});
+
+RoomSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    chatHistory: {
+      type: [MessageSchema],
+      required: true,
+      default: [],
+    },
+  },
+  { timestamps: true }
+);
+
+export default model('rooms', RoomSchema);
