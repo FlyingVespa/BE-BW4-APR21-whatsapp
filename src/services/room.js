061426b4d4ee6ded8e3e13673d86ade2ca2d0c09
ArturSzmidt <80460700+ArturSzmidt@users.noreply.github.com>
2021-09-07 16:46:26 +0200

    need fix
    fix /me

--- src/services/room.js
+++ src/services/room.js
@@ -1,19 +1,30 @@
 import express from 'express';
+import { JWTAuthMiddleware } from '../auth/middlewares.js';
 import RoomModel from '../models/roomSchema.js';
 
 const roomRouter = express.Router();
 
 // GET
 
-// ??? path check~~
-roomRouter.get('/:id', async (req, res) => {
+roomRouter.get('/', async (req, res, next) => {
   try {
     const room = await RoomModel.findById(req.params.id);
     res.status(200).send({ messages: asd });
   } catch (error) {
     console.log(error);
+    next(error);
   }
 });
+
+// ??? path check~~
+// roomRouter.get('/:id', async (req, res) => {
+//   try {
+//     const room = await RoomModel.findById(req.params.id);
+//     res.status(200).send({ messages: asd });
+//   } catch (error) {
+//     console.log(error);
+//   }
+// });
 
 // POST
 roomRouter.post('/', async (req, res, next) => {
@@ -27,6 +38,19 @@ roomRouter.post('/', async (req, res, next) => {
   } catch (error) {
     console.log(error);
     error.status = 400;
+    next(error);
+  }
+});
+
+roomRouter.get('/me', JWTAuthMiddleware, async (req, res, next) => {
+  try {
+    console.log('HELLLO THIS IS MY CONSOLE LOG!!!', req.user._id.toString());
+    const find = RoomModel.find({ participants: req.user._id.toString() });
+    console.log('FIND', find);
+    console.log('participants', participants);
+    res.send(find);
+  } catch (error) {
+    console.log(error);
     next(error);
   }
 });
