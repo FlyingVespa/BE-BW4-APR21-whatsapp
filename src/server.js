c72b91a0e2beb9da885cc7090123116db4ed5e72
Samuel Obeng <61053627+samlobeng@users.noreply.github.com>
2021-09-08 17:41:02 +0200

    fixes

--- src/server.js
+++ src/server.js
@@ -1,22 +1,22 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import cors from 'cors';
-import listEndpoints from 'express-list-endpoints';
-import dotenv from 'dotenv';
-import { Server } from 'socket.io';
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import listEndpoints from "express-list-endpoints";
+import dotenv from "dotenv";
+import { Server } from "socket.io";
 import { createServer } from "http";
-import UserModel from './models/userSchema.js'
-import RoomModel from './models/roomSchema.js'
+import UserModel from "./models/userSchema.js";
+import RoomModel from "./models/roomSchema.js";
 // ERROR HANDLER import
 import {
   forbiddenErrHandler,
   serverErrHandler,
   badReqErrHandler,
   notFoundErrHandler,
-} from './errorHandlers.js';
-import usersRouter from './auth/auth.js';
-import userRouter from './services/users.js';
-import roomRouter from './services/room.js';
+} from "./errorHandlers.js";
+import usersRouter from "./auth/auth.js";
+import userRouter from "./services/users.js";
+import roomRouter from "./services/room.js";
 
 // CHAT ROUTER import
 const PORT = process.env.PORT || 4545;
@@ -26,15 +26,14 @@ app.use(express.json());
 
 dotenv.config();
 
-
 const server = createServer(app);
 // const server = createServer;
 // const io = new Server(server, { true})
 const io = new Server(server, { allowEIO3: true });
 // app.use(router? user )
-app.use('/auth', usersRouter);
-app.use('/users', userRouter);
-app.use('/room', roomRouter);
+app.use("/auth", usersRouter);
+app.use("/users", userRouter);
+app.use("/room", roomRouter);
 // app.use('/', messageRouter);
 
 // app.use( errohandlers)
@@ -43,57 +42,57 @@ app.use(notFoundErrHandler);
 app.use(forbiddenErrHandler);
 app.use(serverErrHandler);
 
-export const sockets = {}
-let onlineUsers = []
+export const sockets = {};
 
-io.on('connection', (socket) => {
-  socket.on('did-connect', async (userId) => {
-    sockets[userId] = socket
+io.on("connection", (socket) => {
+  socket.on("did-connect", async (userId) => {
+    sockets[userId] = socket;
     try {
-      const rooms = await RoomModel.find({ participants: userId })
-      for (let room of rooms) socket.join(room._id.toString())
+      const rooms = await RoomModel.find({ participants: userId });
+      for (let room of rooms) socket.join(room._id.toString());
     } catch (error) {
-      console.log('error:', error)
+      console.log("error:", error);
     }
-  })
+  });
 
   socket.on("joinRoom", async (roomId) => {
     socket.join(roomId);
   });
 
   socket.on("sendMessage", async ({ message, roomId }) => {
-    socket.join(roomId)
-    const room = await RoomModel.findById(roomId)
+    socket.join(roomId);
+    const room = await RoomModel.findById(roomId);
 
-    await RoomModel.findOneAndUpdate({ _id: roomId }, {
-      $push: { chatHistory: message }
-    })
-    socket.to(roomId).emit("message", message)
-  })
+    await RoomModel.findOneAndUpdate(
+      { _id: roomId },
+      {
+        $push: { chatHistory: message },
+      }
+    );
+    socket.to(roomId).emit("message", message);
+  });
 
   socket.on("login", ({ username, room }) => {
-    onlineUsers.push({ username, id: socket.id, room })
+    onlineUsers.push({ username, id: socket.id, room });
 
-    socket.join(room)
-    console.log(socket.rooms)
+    socket.join(room);
+    console.log(socket.rooms);
 
     // Emits to everyone excluding this client
-    socket.broadcast.emit("newLogin")
-    socket.emit("loggedin")
-})
+    socket.broadcast.emit("newLogin");
+    socket.emit("loggedin");
+  });
 
   socket.on("disconnect", (userId) => {
-    delete sockets[userId]
-    console.log("disconnected")
-  })
-})
-
-
+    delete sockets[userId];
+    console.log("disconnected");
+  });
+});
 
 mongoose.connect(process.env.MONGODB_CONNECT).then(() => {
-  console.log('SUCCESS: connected to MONGODB');
+  console.log("SUCCESS: connected to MONGODB");
   app.listen(PORT, () => {
     listEndpoints(app);
-    console.log('SERVER listening on: ' + PORT);
+    console.log("SERVER listening on: " + PORT);
   });
 });
--- src/services/users.js
+++ src/services/users.js
@@ -2,7 +2,7 @@ import express from "express";
 import multer from "multer";
 import createError from "http-errors";
 import { JWTAuthMiddleware } from "../auth/middlewares.js";
-import UserSchema from "../models/userSchema.js";
+import UserModel from "../models/userSchema.js";
 import { JWTAuthenticate } from "../auth/tools.js";
 import { pipeline } from "stream"
 import { v2 as cloudinary } from "cloudinary"
@@ -24,7 +24,7 @@ const userRouter = express.Router();
 userRouter.put("/:id", JWTAuthMiddleware, async (req, res, next) => {
   try {
       const userId = req.params.id
-    const updatedUser = await UserSchema.findByIdAndUpdate(req.params.id, req.body, {
+    const updatedUser = await UserModel.findByIdAndUpdate(req.params.id, req.body, {
         new: true,
         runValidators: true,
     });
@@ -44,9 +44,11 @@ userRouter.put("/:id", JWTAuthMiddleware, async (req, res, next) => {
   }
 });
 
+
+
 userRouter.post("/:id/upload", JWTAuthMiddleware, uploadOnCloudinary,  async (req, res, next) => {
     try {
-      const user = await UserSchema.findById(req.user._id);
+      const user = await UserModel.findById(req.user._id);
       user.avatar = req.file.path;
       await user.save();
       res.send(user.avatar);
@@ -55,11 +57,31 @@ userRouter.post("/:id/upload", JWTAuthMiddleware, uploadOnCloudinary,  async (re
     }
   });
 
-  userRouter.get("/:username", JWTAuthMiddleware, async (req, res, next) => {
+  userRouter.get("/", JWTAuthMiddleware, async (req, res, next) => {
     try {
-      const regex = new RegExp(req.params.username, "i")
+      const response = await UserModel.find();
+      res.status(201).send(response);
+    } catch (error) {
+      console.log(error);
+      next(error);
+    }
+  });
+  
+  userRouter.get("/me", JWTAuthMiddleware, async (req, res, next) => {
+    try {
+      // console.log(req.user);
+      res.send(req.user);
+    } catch (error) {
+      console.log(error);
+      next(error);
+    }
+  });
+  
+  userRouter.get("/search/:query", JWTAuthMiddleware, async (req, res, next) => {
+    try {
+      const regex = new RegExp(req.params.query, "i")
       // console.log(regex)
-      const users = await UserSchema.find({ username: { $regex: regex } })
+      const users = await UserModel.find({ username: { $regex: regex } })
   
       // console.log(req.params.query)
       // console.log(users)
@@ -71,10 +93,31 @@ userRouter.post("/:id/upload", JWTAuthMiddleware, uploadOnCloudinary,  async (re
       next(error);
     }
   });
-
-  userRouter.put("/:id/status", JWTAuthMiddleware, async (req, res, next) => {
+  
+  userRouter.delete("/me", JWTAuthMiddleware, async (req, res, next) => {
     try {
-      const user = await UserSchema.findById(req.user._id);
+      await req.user.deleteOne();
+    } catch (error) {
+      next(error);
+    }
+  });
+  
+  userRouter.post("/me/setUsername", JWTAuthMiddleware, async (req, res, next) => {
+    try {
+      const user = await UserModel.findById(req.user._id);
+      user.username = req.body.username;
+      await user.save();
+      console.log(user);
+      res.send(user);
+    } catch (error) {
+      console.log(error);
+      next(error);
+    }
+  });
+  
+  userRouter.put("/me/status", JWTAuthMiddleware, async (req, res, next) => {
+    try {
+      const user = await UserModel.findById(req.user._id);
       user.status = req.body.status;
       await user.save();
       res.send(user);
@@ -83,33 +126,6 @@ userRouter.post("/:id/upload", JWTAuthMiddleware, uploadOnCloudinary,  async (re
       next(error);
     }
   });
-
-  userRouter.put("/:id/status", JWTAuthMiddleware, async (req, res, next) => {
-    try {
-      const user = await UserSchema.findById(req.user._id);
-      user.status = req.body.status;
-      await user.save();
-      res.send(user);
-    } catch (error) {
-      console.log(error);
-      next(error);
-    }
-  });
-
-userRouter.delete("/:id",JWTAuthMiddleware, async (req, res, next) => {
-    try {
-      const deletedUser = await UserSchema.findByIdAndDelete(req.params.id)
-      if (deletedUser) {
-        res.status(204).send()
-      } else {
-        next(createError(404, `User with _id ${req.params.id} not found!`))
-      }
-      res.status(204).send()
-    } catch (error) {
-      next(error)
-    }
-  })
-
 export default userRouter;
 // POST
 
